2ffd6b78f11a4e68251f4d2e44f63694416b5a09
V-Kovalenko <gorsetis@yandex.ru>
2022-10-13 00:38:14 +0700

    add new text in develop

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     // AUTHORIZATION
     // LOADING
     //add-redux
+    // add text develop
 const [state] = useState({})
     return (
         <div className='flex-main'>
