3ff8fe76bc548e3cda430aac7add6fd1e8ae89a8
V-Kovalenko <gorsetis@yandex.ru>
2022-10-13 00:52:12 +0700

    '????'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
     // AUTHORIZATION
     // LOADING
     //add-redux
+<<<<<<< HEAD
     // add new text redux
+=======
+    // add text develop
+>>>>>>> develop
 const [state] = useState({})
     return (
         <div className='flex-main'>
